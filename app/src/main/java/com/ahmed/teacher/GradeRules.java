99e4e55886df21a94d7215ceede73517119c16c5
ahmed200442 <a7medrk890@gmail.com>
2026-08-30 17:26:09 +0300

    Document grading rules for version 1.2.0

---
+++ app/src/main/java/com/ahmed/teacher/GradeRules.java
@@ -0,0 +1,14 @@
+package com.ahmed.teacher;
+
+public final class GradeRules {
+    public static final int HOMEWORK_MAX=5;
+    public static final int CLASSWORK_MAX=5;
+    public static final int WEEKLY_MAX=10;
+    public static final int BEHAVIOR_MAX=5;
+    public static final int ASSESSMENTS_TOTAL=25;
+    public static final int EXAM1_MAX=15;
+    public static final int EXAM2_MAX=15;
+    public static final int EXAMS_AVERAGE_MAX=15;
+    public static final int FINAL_TOTAL=40;
+    private GradeRules(){}
+}
